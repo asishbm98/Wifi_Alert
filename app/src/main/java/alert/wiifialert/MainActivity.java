931a0bf299d306398a25ca783b6294c1e4bc2659
Asish Binu Mathew <asishbm98@gmail.com>
2018-10-23 01:30:27 +0530

    Shows connected wifi's ssid

--- app/src/main/java/alert/wiifialert/MainActivity.java
+++ app/src/main/java/alert/wiifialert/MainActivity.java
@@ -1,5 +1,7 @@
 package alert.wiifialert;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,13 +29,20 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.ssid);
         ConnectivityManager connectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         wifiCheck = connectionManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService (Context.WIFI_SERVICE);
+        WifiInfo info = wifiManager.getConnectionInfo ();
+        String ssid_  = info.getSSID();
+
+
+
         if(wifiCheck.isConnected()) {
             android.widget.Toast.makeText(this, "Wifi is connected", Toast.LENGTH_LONG).show();
         }
         else {
             android.widget.Toast.makeText(this, "Wifi is not connected", Toast.LENGTH_LONG).show();
         }
-        //textView.setText("works!!");
+        textView.setText(ssid_);
 
     }
 }
